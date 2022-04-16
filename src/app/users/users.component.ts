bdae1f4cb034c2a01c0469d423fb062482edfc20
maureen-james <manjax97@gmail.com>
2022-04-16 19:33:36 +0300

    define the user constructor" "

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,15 +1,61 @@
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-users',
+//   templateUrl: './users.component.html',
+//   styleUrls: ['./users.component.css']
+// })
+// export class UsersComponent implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit(): void {
+//   }
+
+// }
 import { Component, OnInit } from '@angular/core';
+import {SearchRequestService} from '../search-request.service';
+import {Repository} from '../repository';
+import {User} from '../user';
+
 
 @Component({
-  selector: 'app-users',
-  templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+    providers: [SearchRequestService],
+  styleUrls: ['./search.component.css']
 })
-export class UsersComponent implements OnInit {
 
-  constructor() { }
+export class SearchComponent implements OnInit {
+    public searchMe = 'wanjiiru';
+    public githubUser: string;
 
-  ngOnInit(): void {
+    users: User ;
+    repository: Repository;
+    public searchRepo: string;
+    public resultCount = 12;
+
+
+    findUser(username) {
+        this.githubUser = '';
+        this.searchMe  = username;
+        this.ngOnInit();
+    }
+
+
+  constructor(public githubUserRequest: SearchRequestService, public userRepos: SearchRequestService) { }
+
+  ngOnInit() {
+      this.githubUserRequest.githubUser(this.searchMe);
+      this.users = this.githubUserRequest.users;
+      this.userRepos.gitUserRepos(this.searchMe);
+      console.log(this.userRepos);
   }
 
+
+    searchRepos() {
+        this.searchRepo = '';
+        this.resultCount = 10;
+
+    }
 }
