964d45dffb987356e1e4ce973b1dceb8914ab352
maureen-james <manjax97@gmail.com>
2022-04-17 22:31:12 +0300

    defined the parameters

--- src/app/users.ts
+++ src/app/users.ts
@@ -1,5 +1,15 @@
-export class Users {
+// export class Users {
 
-        constructor(public name:string, public login: string, public avatar_url:string, public public_repos:number , public html_url: string,public created_at:Date, public followers:number ,public following:number){
-        }
-}
+//         constructor(public name:string, public login: string, public avatar_url:string, public public_repos:number , public html_url: string,public created_at:Date, public followers:number ,public following:number){
+//         }
+// }
+export class Users {
+        login: any;
+        bio: any;
+        name: any;
+        company: any;
+        location: any;
+        followers: any;
+        following: any;
+        public_repos:any;
+    }
