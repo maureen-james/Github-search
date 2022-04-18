fc80e0c1163e96ef78dd23a504e56ccc61d7d9bb
maureen-james <manjax97@gmail.com>
2022-04-18 22:08:29 +0300

    polish the project

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,12 +1,3 @@
-// // import { Component, OnInit } from '@angular/core';
-// // import { ApiService } from '../api.service';
-// // @Component({
-// //   selector: 'app-users',
-// //   templateUrl: './users.component.html',
-// //   styleUrls: ['./users.component.css']
-// // })
-// // export class UsersComponent implements OnInit {
-
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
 
